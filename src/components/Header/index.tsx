6484826a8669087c321fff737b8619dea698b2b7
Franklin <franklinteixeira205@gmail.com>
2023-06-22 14:27:56 -0300

    feat: new nav header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 
 const Header: React.FC = () => {
     return (
-        <header className="w-full py-10 px-12 flex items-center gap-4 uppercase text-sm font-medium border-b border-gray-900">
-            <Link href="/">RELEVANTES</Link>
-            <Link href="/recent">RECENTES</Link>
+        <header className="w-full py-10 px-12 flex items-center justify-between uppercase text-sm font-medium border-b border-gray-900">
+            <nav className="flex items-center justify-center gap-4">
+                <Link href="/">RELEVANTES</Link>
+                <Link href="/recent">RECENTES</Link>
+            </nav>
+
+            <nav className="flex items-center justify-center gap-4">
+                <Link href="/signin">ENTRAR</Link>
+            </nav>
         </header>
     );
 };
