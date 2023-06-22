eae1aca69cb795530eff511f92604df4dd8656b1
Franklin <franklinteixeira205@gmail.com>
2023-06-22 15:21:00 -0300

    feat: simple page signin

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,14 +1,16 @@
+import React from 'react';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
-import React from 'react';
+import QuestionLink from '@/components/QuestionLink';
 
 const SigninPage: React.FC = () => {
     return (
         <main>
-            <form className="w-full max-w-xl mt-10 mx-auto space-y-4">
+            <form className="w-full max-w-lg mt-10 mx-auto space-y-4">
                 <Input type="email" placeholder="Email"/>
                 <Input type="password" placeholder="Senha"/>
                 <Button>Entrar</Button>
+                <QuestionLink href="/signup" question="Ainda não possui uma conta?">Criar</QuestionLink>
             </form>
         </main>
     );
