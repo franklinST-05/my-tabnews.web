c1f0d20e9300a12193d86db3fd8fa74e0294bb9d
Franklin <franklinteixeira205@gmail.com>
2023-06-22 10:27:09 -0300

    refactor: created card news component

---
+++ src/components/CardNews/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+
+const CardNews: React.FC = () => {
+    return (
+        <div className="w-full max-w-4xl space-y-2">
+            <div>
+                <time className="text-sm font-medium text-gray-500">Mar 30 de 03</time>
+                <Link href="/" className="block text-xl w-max max-w-full font-bold border-b-2 border-transparent hover:border-gray-700 line-clamp-1">Nova linguagem promete resolver todos os problemas</Link>
+            </div>
+            <p className="text-sm text-gray-400 line-clamp-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis et voluptatibus dignissimos est rem nisi expedita perferendis dicta illo libero consequatur iusto, similique natus accusamus autem! Alias iusto neque porro Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa architecto ab doloribus ipsum fugit pariatur provident quos minima. Blanditiis, eum similique! Tempore, distinctio exercitationem repellat non consequatur vero saepe?</p>
+        </div>
+    );
+}
+
+export default CardNews;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Banner from '@/components/Banner';
+import CardNews from '@/components/CardNews';
 import Header from '@/components/Header';
 import Link from 'next/link';
 import React from 'react';
@@ -11,15 +12,8 @@ const HomePage: React.FC = () => {
             <Banner />
 
             <section className="w-full py-8 space-y-8">
-
-                <div className="w-full max-w-4xl space-y-2">
-                    <div>
-                        <time className="text-sm font-medium text-gray-500">Mar 30 de 03</time>
-                        <Link href="/" className="block text-xl w-max max-w-full font-bold border-b-2 border-transparent hover:border-gray-700 line-clamp-1">Nova linguagem promete resolver todos os problemas</Link>
-                    </div>
-                    <p className="text-sm text-gray-400 line-clamp-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis et voluptatibus dignissimos est rem nisi expedita perferendis dicta illo libero consequatur iusto, similique natus accusamus autem! Alias iusto neque porro Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa architecto ab doloribus ipsum fugit pariatur provident quos minima. Blanditiis, eum similique! Tempore, distinctio exercitationem repellat non consequatur vero saepe?</p>
-                </div>
-
+                <CardNews/>
+                <CardNews/>
             </section>
 
         </main>
