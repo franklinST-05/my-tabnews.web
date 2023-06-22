52542d04cdb0efe4f0c9601be092b6923a365bb5
Franklin <franklinteixeira205@gmail.com>
2023-06-22 09:11:40 -0300

    feat: migrate app to pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <>
+            <Head>
+                <title>Tabnews</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
