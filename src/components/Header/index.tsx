d4ee7d9d815e6fd261ff097d3f3f36e1382dbeae
Franklin <franklinteixeira205@gmail.com>
2023-06-22 10:54:38 -0300

    feat: page recents news

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     return (
         <header className="w-full py-10 flex items-center gap-4 uppercase text-sm font-medium border-b border-gray-900">
             <Link href="/">RELEVANTES</Link>
-            <Link href="/">RECENTES</Link>
+            <Link href="/recent">RECENTES</Link>
         </header>
     );
 };
--- src/pages/recent/index.tsx
+++ src/pages/recent/index.tsx
@@ -1,22 +1,18 @@
-import Banner from '@/components/Banner';
 import CardNews from '@/components/CardNews';
 import Header from '@/components/Header';
 import React from 'react';
 
-const HomePage: React.FC = () => {
+const RecentPage: React.FC = () => {
     return (
         <main>
-
             <Header />
-            <Banner />
 
             <section className="w-full py-8 space-y-8">
-                <CardNews/>
-                <CardNews/>
+                <CardNews />
+                <CardNews />
             </section>
-
         </main>
     );
 };
 
-export default HomePage;
+export default RecentPage;
