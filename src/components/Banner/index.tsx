889c06edd42a38cd5192ab53ded1d12adba47bb7
Franklin <franklinteixeira205@gmail.com>
2023-06-23 13:13:20 -0300

    feat: input search banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Input from '../Input';
 
 const Banner: React.FC = () => {
     return (
-        <section className="w-full py-10">
-            <h1 className="text-6xl font-extrabold" translate="no">TABNEWS</h1>
-            <p className="text-base text-gray-400">Os melhores assuntos!</p>
+        <section className="w-full py-10 space-y-4">
+            <div>
+                <h1 className="text-6xl font-extrabold" translate="no">TABNEWS</h1>
+                <p className="text-base text-gray-400">Os melhores assuntos!</p>
+            </div>
+            <div className="max-w-xl">
+                <Input placeholder="Pesquisar"/>
+            </div>
         </section>
     );
 };
