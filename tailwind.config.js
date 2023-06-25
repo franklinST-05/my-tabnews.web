18d157f70f28c6bc2dd062a0d80b652b7b213f71
Franklin <franklinteixeira205@gmail.com>
2023-06-25 10:45:53 -0300

    feat: abreviation colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#22f',
+                secondary: '#1F2937',
+                'secondary-ab': '#111827',
+                success: '#2f2',
+                danger: '#f22',
+                warn: '#ff1',
+                info: '#58f',
+                light: '#fff',
+                dark: '#000',
+            },
+            textColor: {
+                desc: '#668'
+            }
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
