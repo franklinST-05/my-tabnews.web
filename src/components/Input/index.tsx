a07dc402f031f073031864986c288cf1d3161e0c
Franklin <franklinteixeira205@gmail.com>
2023-06-27 19:38:06 -0300

    feat: add message error

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,28 @@
 import React, { InputHTMLAttributes } from 'react';
 
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    error?: string;
+}
+
+const Input: React.FC<InputProps> = ({ className, error, ...props }) => {
     return (
-        <input
-            {...props}
-            className={`
-                block w-full
-                p-4 px-4
-                text-sm text-light
-                placeholder:text-light/50 outline-none
-                border border-secondary
-                rounded-lg bg-secondary
-                ${className}
-            `}
-        />
+        <div>
+            <input
+                {...props}
+                className={`
+                    block w-full
+                    p-4 px-4
+                    text-sm text-light
+                    placeholder:text-light/50 outline-none
+                    border border-secondary
+                    rounded-lg bg-secondary
+                    ${className}
+                `}
+            />
+            {error && (
+                <span className="text-sm text-danger line-clamp-1 max-w-max">{error}</span>
+            )}
+        </div>
     );
 };
 
