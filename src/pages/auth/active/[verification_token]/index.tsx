4850b1a7b03402de693d9cad295b8b76e7ee3928
Franklin <franklinteixeira205@gmail.com>
2023-06-27 23:35:00 -0300

    feat: update design

--- src/pages/auth/active/[verification_token]/index.tsx
+++ src/pages/auth/active/[verification_token]/index.tsx
@@ -1,5 +1,7 @@
 import Spinner from '@/components/Spinner';
 import axios, { AxiosError } from 'axios';
+import Head from 'next/head';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
@@ -19,10 +21,10 @@ const ActiveAccountPage: React.FC = () => {
             if (err instanceof AxiosError) {
                 if (err.response?.status === 409) router.push('/auth/signin');
                 router.push('/404');
+            } else {
+                setLoading(false);
+                setMessage('Ops! aconteceu um imprevisto, tente novamente em alguns minutos');
             }
-
-            setLoading(false);
-            setMessage('Ops! aconteceu um imprevisto, tente novamente em alguns minutos');
         }
     };
 
@@ -31,12 +33,20 @@ const ActiveAccountPage: React.FC = () => {
     }, [verification_token]);
 
     return (
-        <main className="flex justify-center">
-            <div className="py-8 max-w-max flex flex-col items-center justify-center">
-                <Spinner loading={loading} hideOnNotLoading />
-                <p className=" py-8 text-base font-medium text-desc">
-                    {message ?? 'Aguarde sua conta esta sendo ativada....'}
-                </p>
+        <main className="flex items-center justify-center text-center">
+            <Head>
+                <title>Verification · Tabnews</title>
+                <link rel="shortcut icon" href="/error_favicon.svg" type="image/x-icon" />
+            </Head>
+
+            <div className="mt-32 flex flex-col items-center">
+                <div className="w-32 h-32 flex items-center justify-center">
+                    <Spinner loading={loading} />
+                </div>
+                <div className="space-y-2">
+                    <h1 className="text-3xl font-bold">{message ?? 'Aguarde sua conta esta sendo ativada'}</h1>
+                    <p className="text-sm text-desc">Não compartilhe quaisquer dados sensiveis</p>
+                </div>
             </div>
         </main>
     );
