9f1c697a71ac6927523264b73e1ef75ce686fd24
Franklin <franklinteixeira205@gmail.com>
2023-06-27 23:35:25 -0300

    feat: confirm page

---
+++ src/pages/auth/signup/confirm/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import Icon from '../../../../../public/favicon.svg';
+
+const ConfirmPage: React.FC = () => {
+    return (
+        <main className="flex items-center justify-center text-center">
+            <Head>
+                <title>Confirm · Tabnews</title>
+                <link rel="shortcut icon" href="/error_favicon.svg" type="image/x-icon" />
+            </Head>
+
+            <div className="mt-32 flex flex-col items-center">
+                <Image src={Icon} height={130} width={130} alt="Tabnews" />
+                <div className="space-y-2">
+                    <h1 className="text-3xl font-bold">Verifique seu email</h1>
+                    <p className="text-sm text-desc">Não compartilhe os dados passados por email com ninguem!</p>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default ConfirmPage;
