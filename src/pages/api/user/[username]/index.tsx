c33811818bb96727a5f2a1c843e28a753cfb9c45
Franklin <franklinteixeira205@gmail.com>
2023-06-27 22:12:15 -0300

    feat: temporary simple endpoint get user

---
+++ src/pages/api/user/[username]/index.tsx
@@ -0,0 +1,28 @@
+import repos from '@/infra/database';
+import { HttpResponse } from '@/protocols/http';
+import routerHandler from '@/utils/router-handler';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+
+const router = createRouter<NextApiRequest, NextApiResponse<HttpResponse>>();
+
+router.get(async (req, res) => {
+    const { username } = req.query;
+
+    const existsUser = await repos.user.findByUsername({ username: username as string });
+    if (!existsUser || !existsUser.verified) {
+        return res.status(404).json({
+            error: 'Usuario não econtrado',
+        });
+    }
+
+    const { name, email, verified } = existsUser;
+
+    return res.status(201).json({
+        data: { 
+            user: { name, email, verified }
+        }
+    });
+});
+
+export default routerHandler(router);
