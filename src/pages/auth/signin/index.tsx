26287073c44ffef860b9f29c74d4e7839850570a
Franklin <franklinteixeira205@gmail.com>
2023-06-27 21:23:01 -0300

    feat: valid form and connect with api

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import QuestionLink from '@/components/QuestionLink';
+import { AuthUserSchema } from '@/schemas/User';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios, { AxiosError } from 'axios';
+import { setCookie } from 'cookies-next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
 
 const SigninPage: React.FC = () => {
+
+    const router = useRouter();
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: zodResolver(AuthUserSchema),
+    });
+
+    const handleSignin = handleSubmit(async (fields) => {
+        try {
+            const { data: { data } } = await axios.post('/api/user/auth/signin', fields);
+            setCookie('authorization', data.token, {
+                secure: true,
+            });
+
+            router.push('/');
+        } catch (err) {
+            if(err instanceof AxiosError) {
+                toast(err.response?.data.error);
+            }
+            toast('Erro interno, tente novamente em alguns minutos');
+        }
+    });
+
     return (
         <main>
             <Head>
                 <title>Login · TabNews</title>
             </Head>
 
-            <form className="w-full max-w-lg mt-10 mx-auto space-y-4">
+            <ToastContainer/>
+
+            <form className="w-full max-w-lg mt-10 mx-auto space-y-4" onSubmit={handleSignin}>
                 <div>
                     <h1 className="font-bold text-3xl">Login</h1>
                     <p className="text-sm text-desc">Crie e compartilhe experiencias</p>
                 </div>
-                <Input type="email" placeholder="Email"/>
-                <Input type="password" placeholder="Senha"/>
+                <Input
+                    {...register('email')}
+                    error={errors['email']?.message as string}
+                    type="email"
+                    placeholder="Email"
+                />
+                <Input
+                    {...register('password')}
+                    error={errors['password']?.message as string}
+                    type="password"
+                    placeholder="Senha"
+                />
                 <Button>Entrar</Button>
-                <QuestionLink 
+                <QuestionLink
                     href="/auth/signup"
                     question="Ainda não possui uma conta?"
                 > Criar </QuestionLink>
