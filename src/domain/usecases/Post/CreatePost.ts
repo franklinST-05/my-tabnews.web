e6ce212aff79976f4ede2a62d0996433f801cb42
Franklin <franklinteixeira205@gmail.com>
2023-06-29 08:35:40 -0300

    feat: interface create post method

---
+++ src/domain/usecases/Post/CreatePost.ts
@@ -0,0 +1,14 @@
+import { PostModel } from '@/domain/models/Post';
+import { UserModel } from '@/domain/models/User';
+
+export interface CreatePostModel {
+    title: string;
+    slug: string;
+    description: string;
+    body: string;
+    user_id: UserModel['id']
+}
+
+export interface CreatePost {
+    create(post: CreatePostModel): Promise<PostModel>
+}
