793055f6fe8a5610959a5d42f7f5481ddd0cc32f
Franklin <franklinteixeira205@gmail.com>
2023-06-29 08:40:50 -0300

    feat: export repo

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,9 +1,12 @@
+import { PostRepository } from './prisma/repositories/PostRepository';
 import { UserRepository } from './prisma/repositories/UserRepository';
 
 const userRepo = new UserRepository();
+const postRepo = new PostRepository();
 
 const repos = {
     user: userRepo,
+    post: postRepo,
 };
 
 export default repos;
