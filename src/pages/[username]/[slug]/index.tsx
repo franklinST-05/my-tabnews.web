d414ef8329ce0332b807e528c123ee42ae47363a
Franklin <franklinteixeira205@gmail.com>
2023-06-30 15:11:06 -0300

    feat: get post with username/slug

--- src/pages/[username]/[slug]/index.tsx
+++ src/pages/[username]/[slug]/index.tsx
@@ -1,32 +1,28 @@
 import Button from '@/components/Button';
 import CardComment from '@/components/CardComment';
 import Input from '@/components/Input';
+import { PostDTO } from '@/dtos/PostDTO';
+import { UserDTO } from '@/dtos/UserDTO';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import React from 'react';
-import { FiCoffee, FiMessageSquare, FiShare2 } from 'react-icons/fi';
+import { FiCoffee, FiShare2 } from 'react-icons/fi';
 
-const NewsPage: React.FC = () => {
+const NewsPage: React.FC<ServerSideResponse> = ({ post, user }) => {
     return (
         <main className="space-y-10">
             <section className="max-w-5xl space-y-10">
                 <div>
                     <div className="py-4">
-                        <h1 className="text-lg text-light">FranklinST-05</h1>
+                        <a href={`/${user.username}`} className="text-lg text-light">{user.name}</a>
                         <p className="text-sm text-desc">200 Cafeinas</p>
                     </div>
                     <div>
                         <div className="py-2">
-                            <h1 className="text-2xl font-bold">⚙️ Essa ferramenta GRÁTIS vai lhe poupar MUITO do seu tempo e os Youtubers não ficarão feliz com isso (Desculpa Mich...Deschamps)</h1>
+                            <h1 className="text-2xl font-bold">{post.title}</h1>
                         </div>
                         <article>
-                            <p>Com certeza, você já deve ter sentido a sensação de que o tempo está passando cada dia mais rápido.</p>
-                            <p>Eu tenho esse sentimento diversas vezes, principalmente quando olho para a minha lista de objetivos que fiz no ano novo que ainda não foram realizados, e a ansiedade começa a me observar pela janela de casa.</p>
-                            <p>É nesses momentos que eu lembro das palavras do poeta, Boca de 09: "Fuja!"</p>
-                            <p>E por conta desse fato, eu sempre busco estar o mais produtivo possível(não que eu seja em 100% do tempo), mesmo que seja um mílimetro a mais que o dia anterior.</p>
-                            <p>Esse hábito me fez encontrar uma ferramenta que parece ter sido forjada pelos anões para os deuses(Eitri, o anão programador, criou junto com o machado de Thor)</p>
-                            <p>A incrível ferramenta se chama VideoHighLight</p>
-                            <p>Ela se baseia em IA para assistir os vídeos em 10X(exatamente isso que você leu) para sumarizar e resumir os trechos de cada vídeo, como se fossem separados em capítulos, e cada capítulo contendo seu resumo.</p>
-                            <p>E quais os benefícios você obtém usando essa ferramenta?</p>
-                            <p>Essa é bem fácil: <b>UM GANHO EXPONENCIAL DE TEMPO</b></p>
+                            { post.body }
                         </article>
                     </div>
                 </div>
@@ -64,5 +60,32 @@ const NewsPage: React.FC = () => {
         </main>
     );
 };
+
+
+interface ServerSideResponse {
+    user: UserDTO;
+    post: PostDTO;
+}
+
+export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async (context) => {
+
+    try {
+        const { username, slug } = context.query;
+        const handleUser = await axios.get(`http://localhost:3000/api/user/${username}`);
+        const handlePost = await axios.get(`http://localhost:3000/api/post/${slug}`);
+
+        const user = handleUser.data.data.user;
+        const post = handlePost.data.data.post;
+
+        return {
+            props: { user, post }
+        };
+
+    } catch (err) {
+        return { notFound: true };
+    }
+
+};
+
 
 export default NewsPage;
