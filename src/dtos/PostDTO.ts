79dba247e1dab8d2d08f5d1bcd0a79095ba0bdd8
Franklin <franklinteixeira205@gmail.com>
2023-06-30 15:07:31 -0300

    feat: update post dto

--- src/dtos/PostDTO.ts
+++ src/dtos/PostDTO.ts
@@ -1,3 +1,5 @@
+import { FindAllPostResponse } from '@/domain/usecases/Post';
+
 export interface PostDTO {
     id: string;
     title: string;
@@ -6,9 +8,16 @@ export interface PostDTO {
     body: string;
 }
 
+export interface PostDTOIncludesUser extends PostDTO {
+    User: {
+        name: string;
+        username: string;
+    }
+}
+
 export class PostDTO {
-    
-    public constructor({id, title, slug, description, body}: PostDTO) {
+
+    public constructor({ id, title, slug, description, body }: PostDTO) {
         this.id = id;
         this.title = title;
         this.slug = slug;
@@ -18,5 +27,17 @@ export class PostDTO {
 
     public static fromArray(array: Array<PostDTO | unknown>): Array<PostDTO> {
         return array.map((post) => new PostDTO(post as PostDTO));
+    }
+
+    public static fromArrayIncludesUser(array: Array<FindAllPostResponse>): Array<PostDTOIncludesUser> {
+        return array.map((p) => {
+            const post = new PostDTO(p as PostDTO);
+            const user = {
+                name: p.User.name,
+                username: p.User.username,
+            };
+
+            return { ...post, User: user };
+        });
     }
 }
