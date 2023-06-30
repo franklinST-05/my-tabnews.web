ef27b971d87613a537c11b131f7f23b532855a5e
Franklin <franklinteixeira205@gmail.com>
2023-06-30 15:10:14 -0300

    feat: update properties card news post

--- src/components/CardNews/index.tsx
+++ src/components/CardNews/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 
-const CardNews: React.FC = () => {
+interface CardNewsProps {
+    key: number;
+    slug: string;
+    name: string;
+    title: string;
+    username: string;
+    description: string;
+}
+
+const CardNews: React.FC<CardNewsProps> = ({ key, slug, title, name, username, description }) => {
     return (
-        <div className="w-full max-w-4xl space-y-2">
+        <div className="w-full max-w-4xl" key={key}>
             <div>
-                <time className="text-sm font-medium text-desc">Mar 30 de 03</time>
-                <Link href="/franklinST-05/nova-linguagem-promete-resolver-todos-os-problemas" className="block text-xl w-max max-w-full font-bold border-b-2 border-transparent hover:border-secondary line-clamp-1">Nova linguagem promete resolver todos os problemas</Link>
+                <p className="text-sm font-medium text-desc">{name}</p>
+                <Link href={`/${username}/${slug}`} className="block text-xl w-max max-w-full font-bold border-b-2 border-transparent hover:border-secondary line-clamp-1">
+                    {title}
+                </Link>
             </div>
-            <p className="text-sm text-desc line-clamp-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis et voluptatibus dignissimos est rem nisi expedita perferendis dicta illo libero consequatur iusto, similique natus accusamus autem! Alias iusto neque porro Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam culpa architecto ab doloribus ipsum fugit pariatur provident quos minima. Blanditiis, eum similique! Tempore, distinctio exercitationem repellat non consequatur vero saepe?</p>
+            <p className="text-sm text-desc line-clamp-2">{description}</p>
         </div>
     );
 };
