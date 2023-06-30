236370adf9f14d26c4ad0ae21b2f2033334f060d
Franklin <franklinteixeira205@gmail.com>
2023-06-30 08:16:35 -0300

    feat: post dto method fromArray

--- src/dtos/PostDTO.ts
+++ src/dtos/PostDTO.ts
@@ -7,11 +7,16 @@ export interface PostDTO {
 }
 
 export class PostDTO {
+    
     public constructor({id, title, slug, description, body}: PostDTO) {
         this.id = id;
         this.title = title;
         this.slug = slug;
         this.description = description;
         this.body = body;
+    }
+
+    public static fromArray(array: Array<PostDTO | unknown>): Array<PostDTO> {
+        return array.map((post) => new PostDTO(post as PostDTO));
     }
 }
