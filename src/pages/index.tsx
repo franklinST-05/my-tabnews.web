0901396eb66eff1387a0796f1dd039ba4b576f81
Franklin <franklinteixeira205@gmail.com>
2023-06-30 15:09:54 -0300

    feat: page index get all posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,58 @@
 import Banner from '@/components/Banner';
 import CardNews from '@/components/CardNews';
+import { PostDTOIncludesUser } from '@/dtos/PostDTO';
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import React from 'react';
+import { FiAlertCircle } from 'react-icons/fi';
 
-const HomePage: React.FC = () => {
+const HomePage: React.FC<ServerSideResponse> = ({ posts, error }) => {
     return (
         <main>
             <Banner />
             <section className="w-full py-8 space-y-8">
-                <CardNews/>
-                <CardNews/>
+                {error && (
+                    <div className="flex items-center gap-2 text-danger animate-bounce">
+                        <FiAlertCircle />
+                        <h1 className="text-sm">{error}</h1>
+                    </div>
+                )}
+
+                {posts.map((post, index) => (
+                    <CardNews
+                        key={index}
+                        slug={post.slug}
+                        title={post.title}
+                        name={post.User.name}
+                        username={post.User.username}
+                        description={post.description}
+                    />
+                ))}
             </section>
         </main>
     );
+};
+
+interface ServerSideResponse {
+    posts: PostDTOIncludesUser[],
+    error?: string;
+}
+
+export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async () => {
+    try {
+        const { data: { data } } = await axios.get('http://127.0.0.1:3000/api/post');
+        
+        return {
+            props: { posts: data.posts }
+        };
+    } catch (err) {
+        return {
+            props: {
+                posts: [],
+                error: 'Erro ao carregar a pagina verifique sua internet'
+            }
+        };
+    }
 };
 
 export default HomePage;
