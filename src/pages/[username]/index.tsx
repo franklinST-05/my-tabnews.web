bd0e43c90ce6e4c0fbea4efa178ab39b3737825b
Franklin <franklinteixeira205@gmail.com>
2023-06-30 15:10:52 -0300

    feat: get infos by username

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,12 +1,14 @@
 import CardNews from '@/components/CardNews';
+import { PostDTO } from '@/dtos/PostDTO';
+import { UserDTO } from '@/dtos/UserDTO';
 import axios from 'axios';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import React from 'react';
 
 
-const UserPage: React.FC<ServerSideResponse> = ({ user }) => {
-    
+const UserPage: React.FC<ServerSideResponse> = ({ user, posts }) => {
+
     return (
         <main>
             <Head>
@@ -19,30 +21,38 @@ const UserPage: React.FC<ServerSideResponse> = ({ user }) => {
                 </div>
             </section>
             <section className="space-y-8">
-                <CardNews />
-                <CardNews />
+                {posts.map((post, index) => (
+                    <CardNews
+                        key={index}
+                        slug={post.slug}
+                        name={user.name}
+                        title={post.title}
+                        username={user.username}
+                        description={post.description}
+                    />
+                ))}
             </section>
         </main>
     );
 };
 
 interface ServerSideResponse {
-    user: {
-        name: string;
-        username: string;
-        email: string;
-        verified: boolean;
-    }
+    user: UserDTO;
+    posts: PostDTO[];
 }
 
 export const getServerSideProps: GetServerSideProps<ServerSideResponse> = async (context) => {
 
     try {
         const { username } = context.query;
-        const { data } = await axios.get('http://localhost:3000/api/user/' + username);
-        
+        const handleUser = await axios.get(`http://localhost:3000/api/user/${username}`);
+        const handlePosts = await axios.get(`http://localhost:3000/api/user/${username}/posts`);
+
+        const user = handleUser.data.data.user;
+        const posts = handlePosts.data.data.posts;
+
         return {
-            props: { ...data.data }
+            props: { user, posts }
         };
 
     } catch (err) {
